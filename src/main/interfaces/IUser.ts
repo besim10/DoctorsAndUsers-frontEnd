c0269482dfca852627dba58a5b5bff7def136335
besim10 <besimsokoli10@gmail.com>
2022-05-09 15:53:09 +0200

    first commit

---
+++ src/main/interfaces/IUser.ts
@@ -0,0 +1,13 @@
+
+interface IUser {
+  	id?: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	birthdate: string
+	phone: string
+	username: string
+	password?: string;
+}
+
+export default IUser;
